04c10f76348496f3f7f0f427e631f6476dcfe390
nathan hains <nathanhains14@yahoo.com>
2021-03-18 19:22:15 -0400

    commit tree ea9bb48024545193be0cc6e3ee7c8ae51d9e5219

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,45 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do
+    redirect to '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :'recipes/new'
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :'recipes/show'
+  end
+
+
+  post '/recipes' do
+    @recipe = Recipe.new(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    @recipe.save
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :'recipes/edit'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.update(params[:recipe])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe.delete(params[:id])
+    redirect to '/recipes'
+  end
 
 end
